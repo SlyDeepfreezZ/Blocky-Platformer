56fa5535f457663e527cde63ff4f5e5a355f34a6
Fox <DeepfreezZ@outlook.com>
2024-01-15 22:34:09 -0600

    Fixed Variable in Debug Log

--- Blocky Platformer/Assets/Scripts/Player.cs
+++ Blocky Platformer/Assets/Scripts/Player.cs
@@ -16,6 +16,6 @@ public class Player : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal");
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
-        Debug.Log("Velocity = {Rigidbody2D.velocity}");
+        Debug.Log($"Velocity = {rigidbody2D.velocity}");
     }
 }
