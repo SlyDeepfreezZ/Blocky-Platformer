ae6b7d649438405cbedd82024fec83736e101757
Fox <DeepfreezZ@outlook.com>
2024-01-16 12:46:35 -0600

    Added walk aniamtion

--- Blocky Platformer/Assets/Scripts/Player.cs
+++ Blocky Platformer/Assets/Scripts/Player.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float _speed = 10;
+    
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
         Debug.Log($"Velocity = {rigidbody2D.velocity}");
+
+        var animator = GetComponent<Animator>();
+        bool walking = horizontal != 0;
+        animator.SetBool("Walk", walking);
     }
 }
