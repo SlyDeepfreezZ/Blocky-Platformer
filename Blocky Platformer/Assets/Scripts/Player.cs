e26a40b16195efec9f8a59453100d700195fa2eb
Fox <DeepfreezZ@outlook.com>
2024-01-31 15:04:58 -0600

    Added Jump duration

--- Blocky Platformer/Assets/Scripts/Player.cs
+++ Blocky Platformer/Assets/Scripts/Player.cs
@@ -13,10 +13,12 @@ public class Player : MonoBehaviour
     [SerializeField] int _maxJumps = 2;
     [SerializeField] Transform _feet;
     [SerializeField] float _downPull = 5;
+    [SerializeField] float _maxJumpDuration = 0.1f;
 
     Vector2 _startPosition;
     int _jumpsRemaining = 2;
     float _fallTimer;
+    float _jumpTimer;
 
     void Start()
     {
@@ -52,6 +54,13 @@ public class Player : MonoBehaviour
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
             _jumpsRemaining--;
             _fallTimer = 0;
+            _jumpTimer = 0;
+        }
+        else if (Input.GetButton("Fire1") && _jumpTimer <= _maxJumpDuration)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
+            _fallTimer = 0;
+            _jumpTimer += Time.deltaTime;
         }
 
         if (isGrounded)
